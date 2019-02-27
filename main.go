e1d24b0b75cf60fee48f413edb1821bf2f477dab
Kazutomo Yoshihara <1894892+kazuy@users.noreply.github.com>
2019-02-27 22:07:16 +0900

    Dockerでサーバー起動を行う

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello World!!")
+}
+
+func main() {
+  server := http.Server {
+    Addr: ":8080",
+  }
+
+  http.HandleFunc("/hello", hello)
+
+  server.ListenAndServe()
+}
+
